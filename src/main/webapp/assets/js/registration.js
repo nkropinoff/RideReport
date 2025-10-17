42ff44b72529119c91f99f72056f40b3e25c3e44
nkropinoff <nikkropinoff@gmail.com>
2025-10-17 22:33:53 +0300

    feat(assets): add ajax query logic for check email availability

--- src/main/webapp/assets/js/registration.js
+++ src/main/webapp/assets/js/registration.js
@@ -7,3 +7,40 @@ document.addEventListener('DOMContentLoaded', function () {
         if (companyTab) companyTab.click();
     }
 });
+
+$(document).ready(function () {
+    $('.email-check').on('blur', function () {
+       const emailInput = $(this);
+       const emailValue = emailInput.val();
+
+       const form = emailInput.closest('form');
+       const submitButton = form.find('button[type="submit"]');
+
+       if (emailValue.trim() === '') {
+           submitButton.prop('disabled', false);
+           return;
+       }
+
+       $.ajax({
+           url: ctx + '/api/check-email',
+           method: 'GET',
+           data: {email: emailValue},
+           dataType: 'json',
+
+           success: function(response) {
+               if (response.isAvailable) {
+                   emailInput.removeClass('is-invalid')
+                   submitButton.prop('disabled', false);
+               } else {
+                   emailInput.addClass('is-invalid');
+                   submitButton.prop('disabled', true);
+               }
+           },
+
+           error: function() {
+               console.error('Failed email check.');
+               submitButton.prop('disabled', false);
+           }
+       })
+    });
+});
