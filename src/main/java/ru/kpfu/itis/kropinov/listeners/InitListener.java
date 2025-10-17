8e48e6d07ef677b93bd53380b3dbfe963580f363
nkropinoff <nikkropinoff@gmail.com>
2025-10-17 16:20:53 +0300

    feat(listener): add database properties configuration, connection pool, datasource and user dao creations according to DI

--- src/main/java/ru/kpfu/itis/kropinov/listeners/InitListener.java
+++ src/main/java/ru/kpfu/itis/kropinov/listeners/InitListener.java
@@ -1,17 +1,32 @@
 package ru.kpfu.itis.kropinov.listeners;
 
+import ru.kpfu.itis.kropinov.dao.UserDao;
+import ru.kpfu.itis.kropinov.dao.impl.UserDaoImpl;
+import ru.kpfu.itis.kropinov.db.CustomConnectionPool;
+import ru.kpfu.itis.kropinov.db.CustomDataSource;
 import ru.kpfu.itis.kropinov.services.UserService;
 import ru.kpfu.itis.kropinov.services.impl.UserServiceImpl;
+import ru.kpfu.itis.kropinov.utils.PropertiesUtil;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @WebListener
 public class InitListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        UserService userService = new UserServiceImpl();
+
+        Properties dbProperties = PropertiesUtil.getProperties("database.properties");
+        CustomConnectionPool connectionPool = new CustomConnectionPool(
+                dbProperties.getProperty("db.url"), dbProperties.getProperty("db.username"), dbProperties.getProperty("db.password"), Integer.parseInt(dbProperties.getProperty("db.poolsize"))
+        );
+        DataSource dataSource = new CustomDataSource(connectionPool);
+
+        UserDao userDao = new UserDaoImpl(dataSource);
+        UserService userService = new UserServiceImpl(userDao);
         sce.getServletContext().setAttribute("userService", userService);
     }
 
