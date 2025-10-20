3bb4b77a7f788d560d9b3dd64f59a8d6f47ce715
nkropinoff <nikkropinoff@gmail.com>
2025-10-20 09:18:42 +0300

    feat(dao): add findByUserId method to CompanyDao

--- src/main/java/ru/kpfu/itis/kropinov/dao/CompanyDao.java
+++ src/main/java/ru/kpfu/itis/kropinov/dao/CompanyDao.java
@@ -3,8 +3,10 @@ package ru.kpfu.itis.kropinov.dao;
 import ru.kpfu.itis.kropinov.entities.Company;
 
 import java.sql.Connection;
+import java.util.Optional;
 
 public interface CompanyDao {
     Company save(Company company);
     Company saveWithConnection(Company company, Connection connection);
+    Optional<Company> findByUserId(int userId);
 }
--- src/main/java/ru/kpfu/itis/kropinov/dao/impl/CompanyDaoImpl.java
+++ src/main/java/ru/kpfu/itis/kropinov/dao/impl/CompanyDaoImpl.java
@@ -3,7 +3,9 @@ package ru.kpfu.itis.kropinov.dao.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.kpfu.itis.kropinov.dao.CompanyDao;
+import ru.kpfu.itis.kropinov.dto.Result;
 import ru.kpfu.itis.kropinov.entities.Company;
+import ru.kpfu.itis.kropinov.enums.VerifyStatus;
 import ru.kpfu.itis.kropinov.exceptions.DataAccessException;
 
 import javax.sql.DataSource;
@@ -11,6 +13,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 
 public class CompanyDaoImpl implements CompanyDao {
     private static final Logger logger = LoggerFactory.getLogger(UserDaoImpl.class);
@@ -63,4 +66,29 @@ public class CompanyDaoImpl implements CompanyDao {
             throw new DataAccessException("Error while saving company with user_id: " + company.getUserId(), e);
         }
     }
+
+    public Optional<Company> findByUserId(int userId) {
+        String sql = "select * from companies where user_id = ?";
+        try (Connection connection = ds.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, userId);
+
+            ResultSet rs = stmt.executeQuery();
+            Company company = null;
+            if (rs.next()) {
+                company = new Company(
+                        rs.getInt(1),
+                        rs.getInt(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        VerifyStatus.valueOf(rs.getString(5))
+
+                );
+            }
+            return Optional.ofNullable(company);
+        } catch (SQLException e) {
+            logger.error("Error while finding company by userId: {}", userId, e);
+            throw new DataAccessException("Error while finding company by userId: " + userId, e);
+        }
+    };
 }
