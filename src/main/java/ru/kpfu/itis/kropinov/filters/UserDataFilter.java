8e779b3ed4e6357a6ae4fa9a0a45c687a2dbdc55
nkropinoff <nikkropinoff@gmail.com>
2025-10-21 23:10:49 +0300

    feat(filter): add user data filter to set user attribute for every template if exist

---
+++ src/main/java/ru/kpfu/itis/kropinov/filters/UserDataFilter.java
@@ -0,0 +1,30 @@
+package ru.kpfu.itis.kropinov.filters;
+
+import ru.kpfu.itis.kropinov.dto.UserSessionDto;
+import ru.kpfu.itis.kropinov.entities.User;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class UserDataFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpSession session = req.getSession(false);
+
+        if (session != null) {
+            UserSessionDto user = (UserSessionDto) session.getAttribute("user");
+            if (user != null) {
+                req.setAttribute("user", user);
+            }
+        }
+
+        chain.doFilter(req, res);
+    }
+}
