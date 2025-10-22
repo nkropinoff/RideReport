408118d423562debbf2d6c6265afcea1ae53be65
nkropinoff <nikkropinoff@gmail.com>
2025-10-22 21:44:34 +0300

    feat(dto): implement PaginatedResult dto class

---
+++ src/main/java/ru/kpfu/itis/kropinov/dto/PaginatedResult.java
@@ -0,0 +1,27 @@
+package ru.kpfu.itis.kropinov.dto;
+
+import java.util.List;
+
+public class PaginatedResult<T> {
+    private List<T> data;
+    private final int totalPages;
+    private final int currentPage;
+
+    public PaginatedResult(List<T> data, int totalPages, int currentPage) {
+        this.data = data;
+        this.totalPages = totalPages;
+        this.currentPage = currentPage;
+    }
+
+    public List<T> getData() {
+        return data;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+}
