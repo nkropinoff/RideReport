dea2e61c6df660d7e8c883ca51a46efb621372c6
nkropinoff <nikkropinoff@gmail.com>
2025-10-23 23:48:37 +0300

    feat(servlet): add AdminCompaniesServlet

---
+++ src/main/java/ru/kpfu/itis/kropinov/servlets/AdminCompaniesServlet.java
@@ -0,0 +1,21 @@
+package ru.kpfu.itis.kropinov.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/admin/companies")
+public class AdminCompaniesServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("pageTitle", "Панель управления");
+        req.setAttribute("pageId", "companies");
+        req.setAttribute("sectionTitle", "Компании");
+
+        req.getRequestDispatcher("companies.ftl").forward(req, resp);
+    }
+}
