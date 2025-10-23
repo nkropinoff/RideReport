c3faef791f68db17637745b32df298e6f861c83f
nkropinoff <nikkropinoff@gmail.com>
2025-10-23 21:31:01 +0300

    feat(service): add execution of finding and counting companies within one connection for solve potential data race condition

--- src/main/java/ru/kpfu/itis/kropinov/services/impl/CompanyServiceImpl.java
+++ src/main/java/ru/kpfu/itis/kropinov/services/impl/CompanyServiceImpl.java
@@ -1,28 +1,40 @@
 package ru.kpfu.itis.kropinov.services.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.kpfu.itis.kropinov.dao.CompanyDao;
-import ru.kpfu.itis.kropinov.dao.impl.CompanyDocumentDaoImpl;
 import ru.kpfu.itis.kropinov.dto.CompanySortingDto;
 import ru.kpfu.itis.kropinov.dto.PaginatedResult;
 import ru.kpfu.itis.kropinov.entities.Company;
-import ru.kpfu.itis.kropinov.enums.VerifyStatus;
+import ru.kpfu.itis.kropinov.exceptions.DataAccessException;
 import ru.kpfu.itis.kropinov.services.CompanyService;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public class CompanyServiceImpl implements CompanyService {
 
+    private static final Logger logger = LoggerFactory.getLogger(CompanyServiceImpl.class);
     private final CompanyDao companyDao;
+    private final DataSource dataSource;
 
-    public CompanyServiceImpl(CompanyDao companyDao) {
+    public CompanyServiceImpl(DataSource dataSource, CompanyDao companyDao) {
+        this.dataSource = dataSource;
         this.companyDao = companyDao;
     }
 
     @Override
     public PaginatedResult<Company> getCompanies(CompanySortingDto dto) {
-        List<Company> companies = companyDao.findAll(dto.getPage(), dto.getSize(), dto.getSortOrder(), dto.getStatus());
-        int totalCount = companyDao.countAll(dto.getStatus());
-        int totalPages = (int) Math.ceil( (double) totalCount / dto.getSize());
-        return new PaginatedResult<>(companies, totalPages, dto.getPage());
+        try (Connection connection = dataSource.getConnection()) {
+            List<Company> companies = companyDao.findAllWithConnection(dto, connection);
+            int totalCount = companyDao.countAllWithConnection(dto.getStatus(), connection);
+            int totalPages = (int) Math.ceil( (double) totalCount / dto.getSize());
+            return new PaginatedResult<>(companies, totalPages, dto.getPage());
+        } catch (SQLException e) {
+            logger.error("Failed to fetch companies", e);
+            throw new DataAccessException("Failed to fetch companies", e);
+        }
     }
 }
