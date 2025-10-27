5b25e207e353f6519d8761cc104c1a6abc1b4a18
nkropinoff <nikkropinoff@gmail.com>
2025-10-28 00:01:32 +0300

    feat(dao): implement saveRouteWithConnection and saveVehicleForRouteWithConnection methods in RouteDao

--- src/main/java/ru/kpfu/itis/kropinov/dao/RouteDao.java
+++ src/main/java/ru/kpfu/itis/kropinov/dao/RouteDao.java
@@ -1,5 +1,12 @@
 package ru.kpfu.itis.kropinov.dao;
 
+import ru.kpfu.itis.kropinov.entities.Route;
+
+import java.sql.Connection;
+import java.util.List;
+
 public interface RouteDao {
     boolean existsByTransportModeIdAndCityIdAndRouteNumber(int companyId, int cityId, String routeNumber);
+    Route saveRouteWithConnection(Route route, Connection connection);
+    void saveVehicleForRouteWithConnection(int routeId, String vehicle, Connection connection);
 }
--- src/main/java/ru/kpfu/itis/kropinov/dao/impl/RouteDaoImpl.java
+++ src/main/java/ru/kpfu/itis/kropinov/dao/impl/RouteDaoImpl.java
@@ -3,13 +3,11 @@ package ru.kpfu.itis.kropinov.dao.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.kpfu.itis.kropinov.dao.RouteDao;
+import ru.kpfu.itis.kropinov.entities.Route;
 import ru.kpfu.itis.kropinov.exceptions.DataAccessException;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class RouteDaoImpl implements RouteDao {
     private DataSource ds;
@@ -36,6 +34,55 @@ public class RouteDaoImpl implements RouteDao {
         } catch (SQLException e) {
             logger.error("Failed to check route number {} for city {} and transportModeId {}", routeNumber, cityId, transportModeId, e);
             throw new DataAccessException("Failed to check route number existence", e);
+        }
+    }
+
+    @Override
+    public Route saveRouteWithConnection(Route route, Connection connection) {
+        String sql = "INSERT INTO routes (company_id, city_id, transport_mode_id, number) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setInt(1, route.getCompanyId());
+            stmt.setInt(2, route.getCityId());
+            stmt.setInt(3, route.getTransportModeId());
+            stmt.setString(4, route.getRouteNumber());
+
+            int result = stmt.executeUpdate();
+            if (result == 0) {
+                logger.error("Route was not saved, executeUpdate returned 0");
+                throw new DataAccessException("Route was not saved");
+            }
+
+            try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    route.setId(generatedKeys.getInt(1));
+                } else {
+                    logger.error("Route was saved, but no id obtained");
+                    throw new DataAccessException("Route was saved, but no id obtained");
+                }
+            }
+
+            return route;
+        } catch (SQLException e) {
+            logger.error("Error while saving route", e);
+            throw new DataAccessException("Error while saving company", e);
+        }
+    }
+
+    @Override
+    public void saveVehicleForRouteWithConnection(int routeId, String vehicle, Connection connection) {
+        String sql = "INSERT INTO route_vehicles (route_id, vehicle_number) VALUES (?, ?)";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, routeId);
+            stmt.setString(2, vehicle);
+
+            int result = stmt.executeUpdate();
+            if (result == 0) {
+                logger.error("Vehicle: {} for route with id: {} was not saved, executeUpdate returned 0", vehicle, routeId);
+                throw new DataAccessException("Vehicle for route with was not saved");
+            }
+        } catch (SQLException e) {
+            logger.error("Failed saving vehicle: {} for route with id: {}", vehicle, routeId, e);
+            throw new DataAccessException("Failed saving vehicle for route", e);
         }
     }
 }
